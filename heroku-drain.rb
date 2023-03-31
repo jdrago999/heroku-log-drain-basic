6470435314c68e5946ca76932af7b32bdb26572e
John Drago <jdrago.999@gmail.com>
2023-03-30 17:23:36 -0700

    Conditionally parse message as JSON so we send JSON, not a string of JSON

--- heroku-drain.rb
+++ heroku-drain.rb
@@ -22,7 +22,8 @@ end
 post '/(**log_group)/(**log_stream_name)' do
   # Parses log format like this:
   # 89 <45>1 2023-03-18T00:01:28.723822+00:00 host heroku web.1 - State changed from down to up
-  time, message = %r{^(.+?)\s+(.+?)\s+(\S+)\s+(.+?)$}.match(request.body.read).to_a[3..4]
+  time, raw_message = %r{^(.+?)\s+(.+?)\s+(\S+)\s+(.+?)$}.match(request.body.read).to_a[3..4]
+  message = JSON.parse(raw_message) rescue raw_message
   log_event = {
     # AWS Wants timestamps as integers = unix time * 1000:
     timestamp: DateTime.rfc3339(time).to_time.to_i * 1000,
